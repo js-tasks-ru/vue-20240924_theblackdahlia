32f7db6616d0157ad2fee893cf20df52cb65fe26
DaliyaAsel <theblackdahlia@mail.ru>
2024-10-07 16:27:20 +0700

    createApp

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,23 @@
 import { defineComponent, createApp } from 'vue'
+
+const getTodayDate = () => {
+  return new Date().toLocaleDateString(navigator.language, {
+    dateStyle: 'long',
+  });
+};
+
+export const App = defineComponent({
+  name: 'App',
+
+  setup(){
+    const date =  getTodayDate();
+    return {
+      date,
+    }
+  },
+
+  template: `<div>Сегодня {{ date }}</div>`
+})
+
+const app = createApp(App);
+app.mount('#app')
